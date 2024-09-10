c2afc7f759f54b901ca2fd8ada8d2027739d9b69
taileduc0404 <taileduc0404@gmail.com>
2024-09-10 16:50:11 +0700

    Update .gitignore

--- CompressMedia/Controllers/MediaController.cs
+++ CompressMedia/Controllers/MediaController.cs
@@ -1,66 +1,83 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using CompressMedia.DTOs;
 using CompressMedia.Models;
+using CompressMedia.Repositories;
 using CompressMedia.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CustomAuth.Controllers
 {
-    public class MediaController : Controller
-    {
-        private readonly IMediaService _mediaService;
-        private readonly INotyfService _notyfService;
+	public class MediaController : Controller
+	{
+		private readonly IMediaService _mediaService;
+		private readonly IUserService _userService;
+		private readonly INotyfService _notyfService;
 
-        public MediaController(IMediaService imageService, INotyfService notyfService)
-        {
-            _mediaService = imageService;
-            _notyfService = notyfService;
-        }
+		public MediaController(IMediaService imageService, INotyfService notyfService, IUserService userService)
+		{
+			_mediaService = imageService;
+			_notyfService = notyfService;
+			_userService = userService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            IEnumerable<Media> mediaList = await _mediaService.GetAllVideo();
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			IEnumerable<User> users = await _userService.GetAllUser();
+			if (users == null)
+			{
+				return RedirectToAction("AccessDenied");
+			}
+			IEnumerable<Media> mediaList = await _mediaService.GetAllVideo();
 
-            if (mediaList == null)
-            {
-                return RedirectToAction("AccessDenied");
-            }
+			if (mediaList == null)
+			{
+				return RedirectToAction("AccessDenied");
+			}
 
-            // Chuyển đổi danh sách Media sang MediaDto để truyền vào View
-            IEnumerable<MediaDto> mediaDtoList = mediaList.Select(media => new MediaDto
-            {
-                //MediaType = media.MediaType,
-                CreatedDate = media.CreatedDate,
-                //UserId = media.UserId,
-                MediaPath = media.MediaPath,
-                Size = (double)(media.Size / 1048576.0)
-            });
+			// Chuyển đổi danh sách Media sang MediaDto để truyền vào View
+			IEnumerable<MediaDto> mediaDtoList = mediaList.Select(media => new MediaDto
+			{
+				//MediaType = media.MediaType,
+				CreatedDate = media.CreatedDate,
+				//UserId = media.UserId,
+				MediaPath = media.MediaPath,
+				Size = Math.Round((double)(media.Size / 1048576.0), 2)
+			});
 
-            return View(mediaDtoList);
-        }
+			return View(mediaDtoList);
+		}
 
+		public IActionResult AccessDenied()
+		{
+			return View();
+		}
 
-        [HttpGet]
-        public IActionResult Video()
-        {
-            return View();
-        }
+		[HttpGet]
+		public async Task<IActionResult> Video()
+		{
+			IEnumerable<User> users = await _userService.GetAllUser();
+			if (users == null)
+			{
+				return RedirectToAction("AccessDenied");
+			}
+			return View();
+		}
 
-        [HttpPost]
-        [RequestFormLimits(MultipartBodyLengthLimit = 536870912)]
-        public IActionResult UploadVideo(MediaDto mediaDto)
-        {
-            bool result = _mediaService.UploadMedia(mediaDto);
-            if (!result)
-            {
-                _notyfService.Success("Upload image failed.");
-                return RedirectToAction("Index");
-            }
+		[HttpPost]
+		[RequestFormLimits(MultipartBodyLengthLimit = 536870912)]
+		public IActionResult UploadVideo(MediaDto mediaDto)
+		{
+			bool result = _mediaService.UploadMedia(mediaDto);
+			if (!result)
+			{
+				_notyfService.Success("Upload image failed.");
+				return RedirectToAction("Index");
+			}
 
-            _notyfService.Success("Upload image successfully.");
-            return RedirectToAction("Index");
-        }
+			_notyfService.Success("Upload image successfully.");
+			return RedirectToAction("Index");
+		}
 
-    }
+	}
 }
